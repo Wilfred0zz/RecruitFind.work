763617e5ecb92e3b96615929e52e8eca7287d0e8
connieDeng <dengconnie0723@gmail.com>
2020-04-06 22:10:52 -0400

    some html and file structure updates

---
+++ FrontEnd/src/components/Mainpage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import officeImg from '../public/office.jpg';
+
+class Mainpage extends Component {
+  render() {
+    return (
+        <div className = 'main-'page id = 'main'> 
+            <h1 id='catchPhrase'>Don't be a slob, get a job</h1>
+            <img id = 'officePic' src={officeImg}/>
+        </div>
+    )
+  }
+}
+
+export default Mainpage;
