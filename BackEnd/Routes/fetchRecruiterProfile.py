ce195a858a8a5d09caa3e03913d97413873305ba
elijahcano33 <elijah.c104777@gmail.com>
2020-04-22 20:34:48 -0400

    fetchRecruiterInfo route completed and working successfully

---
+++ BackEnd/Routes/fetchRecruiterProfile.py
@@ -0,0 +1,39 @@
+from flask import Flask, Blueprint, request
+import psycopg2
+from passlib.hash import argon2
+import bcrypt
+
+database = psycopg2.connect(user = "postgres", password = "htrvvC56nb02kqtA", host= "34.66.114.193", port = "5432", database = "recruitfindwork")
+
+#creates a cursor object to execute PostgreSQL commands via python
+cursor = database.cursor()
+
+frp = Blueprint('fetchRecruiterProfile', __name__)
+
+@frp.route("/api/fetchRecruiterProfileInfo", methods=["GET"])
+def fetchRecruiterProfileInfo():
+    response = dict()
+
+    token = request.cookies.get('token')
+
+    cursor.execute(f"""SELECT user_id FROM public."Personal Information" WHERE token='{token}'""")
+
+    currentUserId = cursor.fetchone()[0]
+
+    if currentUserId:
+        cursor.execute(f"""SELECT recruiter_company, recruiter_position, recruiter_company_street_address, recruiter_city, recruiter_postal, recruiter_country FROM public."Recruiter Company Information" WHERE user_id='{currentUserId}'""")
+        queryResult = cursor.fetchall()
+        response['recruiter_company'] = queryResult[0][0]
+        response['recruiter_position'] = queryResult[0][1]
+        response['recruiter_company_street_address'] = queryResult[0][2]
+        response['recruiter_city'] = queryResult[0][3]
+        response['recruiter_postal'] = queryResult[0][4]
+        response['recruiter_country'] = queryResult[0][5]
+
+        response['status'] = True
+        response['status_info'] = 'Recruiter Profile Info Stored Successfully'
+    else:
+        response['status'] = False
+        response['status_info'] = 'Invalid token!'
+    
+    return response
