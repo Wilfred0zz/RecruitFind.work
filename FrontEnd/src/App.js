053f481fb91f0f75372c7f008168bbfefcff8142
connieDeng <dengconnie0723@gmail.com>
2020-05-04 10:37:26 -0400

    Recruiter Register Forms

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import MainPage from './components/main_page/MainPage';
 import CandidateRegister from './components/register_candidate/RegisterCandidate';
+import RecruiterRegister from './components/register_recruiter/RegisterRecruiter';
 
 function App() {
   const MainPageComponent = () => <MainPage/>
   const CandidateRegisterComponent = () => <CandidateRegister/>
+  const RecruiterRecruiterComponent = () => <RecruiterRegister/>
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +18,7 @@ function App() {
       <Switch> 
         <Route exact path="/" render={MainPageComponent}/>
         <Route exact path='/register_candidate' render={CandidateRegisterComponent}/>
+        <Route exact path='/register_recruiter' render={RecruiterRecruiterComponent}/>
       </Switch>
 
      
