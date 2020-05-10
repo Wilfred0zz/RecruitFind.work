7dd8625f06536676d8a6ecb45713a27fe5892e67
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-05-09 20:43:12 -0400

    Queries

--- FrontEnd/src/components/all_queries/NewQueriesPage.jsx
+++ FrontEnd/src/components/all_queries/NewQueriesPage.jsx
@@ -1,32 +1,104 @@
 import React, { Component } from 'react';
 import NavigationBarRecruiter from './../recruiter_profile/navigation_bar_recruiter/NavigationBarRecruiter';
-
+import {Link} from 'react-router-dom';
+import Autocomplete from '@material-ui/lab/Autocomplete'; 
+import TextField from '@material-ui/core/TextField';
 
 class NewQueries extends Component{
     constructor(props){
         super(props);
         this.state = { query_title: "",
-        query_description: "",
-        query_payment: "",
-        query_date: "",
-        desired_skill_1: "",
-        desired_skill_2: "",
-        desired_skill_3: "",
-        desired_skill_4: "",
-        desired_skill_5: "",
-        desired_skill_6: "",
-        desired_skill_7: "",
-        desired_skill_8: "",
-        desired_skill_9: "",	
-        desired_skill_10: ""
+          query_description: "",
+          query_payment: "",
+          query_date: "",
+          skills: [],
+          desired_skill_1: "",
+          desired_skill_2: "",
+          desired_skill_3: "",
+          desired_skill_4: "",
+          desired_skill_5: "",
+          desired_skill_6: "",
+          desired_skill_7: "",
+          desired_skill_8: "",
+          desired_skill_9: "",	
+          desired_skill_10: ""
         }
       }
+       refreshPage() {
+        window.location.reload(false);
+      }
+      
       onChange = (event) =>{
         this.setState({
           [event.target.name]: event.target.value
         },  console.log(this.state))
       }
       
+          // To decrease count or increase count based on skill added
+      modifySkill = (event, value, type) => {
+        // value contains the selected inputs over time
+        if(type === 'remove-option'){
+          this.setState({ 
+            // skill_count: this.state.skill_count - 1,
+            skills: value,
+            // skill_options: []
+          });
+        } else if(this.state.skills.length >= 10){
+          // event.target.value='';
+          return alert('Only 10 allowed');
+        } else if((type === 'create-option') && this.state.skills.length < 10){
+          var data = event.target.value.toLowerCase();
+          if(this.state.skills && (this.state.skills.filter(currentValue=>data===currentValue).length > 0)){
+            return;
+          }
+          this.setState({ 
+            // skill_count: this.state.skill_count + 1,
+            skills: [...this.state.skills, value[value.length-1].toLowerCase()],
+            // skill_options: []
+          });
+        } else {
+          return;
+        }
+      }
+
+
+      handleSkillSubmission = async () => {
+        // console.log(this.state)
+        if(!this.state.skills || this.state.skills.length < 1){
+          // return(alert("Please enter atleast one skill"));
+          throw(Error(alert("Please enter atleast one skill")));
+        }
+
+        for(let i = 0; i < this.state.skills.length; i++){
+          // console.log("length: ", this.state.skills.length)
+          const skill = {
+            "skill": this.state.skills[i].toLowerCase(),
+            "is_deleted": false
+          }
+          // console.log
+          const response = await fetch('/api/deleteCandidateSkill',{
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: 'PUT',
+            body: JSON.stringify(skill)
+          })
+          
+          const status = response.status;
+          const result = await response.json();
+
+          if(status >= 400) {
+            console.log(result);
+            throw Error(`error in skill_${i}`);
+          }
+          else{
+            console.log(`Successfully added skill_${i}`);
+          }
+        }
+        
+      }
+
       onSubmit = async(event) =>{
         event.preventDefault();
         try {
@@ -53,8 +125,8 @@ class NewQueries extends Component{
       }
       
     render(){
-      const{query_title, query_description, query_payment, query_date, query_skill_1, query_skill_2, query_skill_3, query_skill_4, query_skill_5, query_skill_6, query_skill_7, query_skill_8, query_skill_9, query_skill_10} =this.state
-      
+      const{query_title, query_description, query_payment, query_date} =this.state
+
       return(
         <div className="NewQuery">
           <NavigationBarRecruiter/>
@@ -68,28 +140,28 @@ class NewQueries extends Component{
                   <input type="text" onChange={this.onChange} value={query_payment} name="query_payment" />
                   <label htmlFor ="query_date">Date </label>
                   <input type="text" onChange={this.onChange} value={query_date} name="query_date" />
-                  <label htmlFor ="query_skill_1"> Skill</label>
-                  <input type="text" onChange={this.onChange} value={query_skill_1} name="query_skill_1" />
-                  <label htmlFor ="query_skill_2"> Skill</label>
-                  <input type="text" onChange={this.onChange} value={query_skill_2} name="query_skill_2" />
-                  <label htmlFor ="query_skill_3">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_3} name="query_skill_3" />
-                  <label htmlFor ="query_skill_4">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_4} name="query_skill_4" />
-                  <label htmlFor ="query_skill_5">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_5} name="query_skill_5" />
-                  <label htmlFor ="query_skill_6">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_6} name="query_skill_6" />
-                  <label htmlFor ="query_skill_7">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_7} name="query_skill_7" />
-                  <label htmlFor ="query_skill_8">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_8} name="query_skill_8" />
-                  <label htmlFor ="query_skill_9">Skill </label>
-                  <input type="text" onChange={this.onChange} value={query_skill_9} name="query_skill_9" />
-                  <label htmlFor ="query_skill_10">Skill</label>
-                  <input type="text" onChange={this.onChange} value={query_skill_10} name="query_skill_10" />
-                  <button>Submit</button>
+                  <Autocomplete
+                    multiple // allows multiple entries                 
+                    limitTags={10} // only 10 displayed in input box                 
+                    freeSolo // add entries not in provided options                 
+                    disableClearable={true} // remove delete all option                 
+                    size='small' // dispaly small                 
+                    id="multiple-limit-tags" // id for this component                 
+                    options={[]} // provide no options
+                    value={this.state.skills}                 
+                    onChange={this.modifySkill} // when a user presses enter this function occurs                 
+                    renderInput={params => ( //display for all entered inputs                   
+                      <TextField 
+                        {...params}                     
+                        variant="outlined"                     
+                        label="skills"                     
+                        placeholder="skill"                   
+                      />                 
+                    )}               
+                  />
+                  <button onClick={this.refreshPage}>Submit</button>
               </form>
+              <Link to = "/all_queries"><button>Cancel</button></Link>
             </div>
         </div>
       )
