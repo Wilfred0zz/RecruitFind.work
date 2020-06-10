fef45e32a5853a15f754857237db95f7ebfedeeb
elijahcano33 <elijah.c104777@gmail.com>
2020-06-10 01:15:42 -0400

    more new server info additions

--- BackEnd/Routes/Candidate/Links/candidateLinks.py
+++ BackEnd/Routes/Candidate/Links/candidateLinks.py
@@ -9,7 +9,7 @@ cl = Blueprint('candidateLinks', __name__)
 @login_required
 def storeCandidateLinks():
     try: 
-        database = psycopg2.connect(user = "postgres", password = "htrvvC56nb02kqtA", host= os.getenv('DATABASE_IP', "172.17.0.1") , port = "5432", database = "recruitfindwork")
+        database = psycopg2.connect(user = "bylinkvsjtfdia", password = "b441303bb98c6533e96fa5c476852dcc067180f3a036d5bde62d61e9c5f19d5f", host= os.getenv('DATABASE_IP', "172.17.0.1") , port = "5432", database = "dauhmnvct04jp4")
         if database:
             cursor = database.cursor()
             response = dict()
